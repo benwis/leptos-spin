bc80b7c1e32ccc85249f51ed91f93633bd88cad6
sjud <64875465+sjud@users.noreply.github.com>
2024-02-08 18:18:19 -0500

    parity with leptos-rs/start-spin'

--- templates/leptos-ssr/content/src/server.rs
+++ templates/leptos-ssr/content/src/server.rs
@@ -1,18 +1,21 @@
-use spin_sdk::http::{ResponseOutparam, IncomingRequest};
+use leptos::ServerFn;
+use leptos_spin::{render_best_match_to_stream, RouteTable, server_fn::register_explicit};
+use spin_sdk::http::{IncomingRequest, ResponseOutparam};
 use spin_sdk::http_component;
-use leptos_spin::{render_best_match_to_stream, RouteTable};
 
 #[http_component]
-async fn handle_{{project-name | snake_case}}(req: IncomingRequest, resp_out: ResponseOutparam) {
+async fn handle_{{crate_name}}(req: IncomingRequest, resp_out: ResponseOutparam) {
     let mut conf = leptos::get_configuration(None).await.unwrap();
-    conf.leptos_options.output_name = "{{project-name | snake_case}}".to_owned();
+    conf.leptos_options.output_name = "{{crate_name}}".to_owned();
 
-    leptos_spin::server_fn::register_explicit::<crate::app::SaveCount>();
+    // Register server functions
+    register_explicit::<crates::pages::home::GetCount>();
+    register_explicit::<crate::pages::home::UpdateCount>();
 
-    let app_fn = crate::app::App;
+    let app_router = crate::routes::AppRouter;
 
-    let mut routes = RouteTable::build(app_fn);
+    let mut routes = RouteTable::build(app_router);
     routes.add_server_fn_prefix("/api").unwrap();
 
-    render_best_match_to_stream(req, resp_out, &routes, app_fn, &conf.leptos_options).await
+    render_best_match_to_stream(req, resp_out, &routes, app_router, &conf.leptos_options).await
 }
